6c06cb7cb50cb7922eaac8375e49e854e95892ec
sravanchekurthi <srsudden@gmail.com>
2025-03-14 12:15:27 -0500

    update

--- backup_files.sh
+++ backup_files.sh
@@ -1,8 +1,10 @@
 #!/bin/bash/
 
+# Define directories and backup destination
 SOURCE_DIR="/home/sravan/Desktop/Sravan"
 BACKUP_DIR="/home/sravan/Desktop/Backups"
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
 
+# Create the backup
 tar -czf "$BACKUP_FILE" -C "$SOURCE_DIR" .
